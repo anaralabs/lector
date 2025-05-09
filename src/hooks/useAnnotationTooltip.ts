b5765dbce033f328a4b451229e37ce8fdc31ec5b
Cristian <raducristiandumitrana@gmail.com>
2025-05-09 18:07:26 +0300

    chore: fix lint

--- src/hooks/useAnnotationTooltip.ts
+++ src/hooks/useAnnotationTooltip.ts
@@ -29,17 +29,6 @@ interface UseAnnotationTooltipReturn {
   getReferenceProps: ReturnType<typeof useInteractions>["getReferenceProps"];
 }
 
-const defaultRect = {
-  width: 0,
-  height: 0,
-  x: 0,
-  y: 0,
-  top: 0,
-  right: 0,
-  bottom: 0,
-  left: 0,
-};
-
 export const useAnnotationTooltip = ({
   annotation,
   onOpenChange,
