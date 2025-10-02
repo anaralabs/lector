62a4785544319ce9628ee3fec2066f5a9042fb7e
Andrei Dorobantu <elfandreis@gmail.com>
2025-10-02 14:55:29 +0300

    feat: memory improvements & safari fixes for rendering (#85)

--- packages/docs/lib/setup.ts
+++ packages/docs/lib/setup.ts
@@ -2,6 +2,6 @@ import "pdfjs-dist/web/pdf_viewer.css";
 import { GlobalWorkerOptions } from "pdfjs-dist";
 
 GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.mjs",
-  import.meta.url
+	"pdfjs-dist/build/pdf.worker.mjs",
+	import.meta.url,
 ).toString();
