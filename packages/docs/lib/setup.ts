b2d080be17def75af51d11c180edf7b540df4eaa
Cristian Crețu <crisemcr@gmail.com>
2026-03-26 16:23:15 +0200

    chore: perf improvements (#105)

--- packages/docs/lib/setup.ts
+++ packages/docs/lib/setup.ts
@@ -1,7 +1,7 @@
 import "pdfjs-dist/web/pdf_viewer.css";
 
 if (typeof window !== "undefined") {
-	import("pdfjs-dist").then(({ GlobalWorkerOptions }) => {
+	import("pdfjs-dist/legacy/build/pdf.mjs").then(({ GlobalWorkerOptions }) => {
 		GlobalWorkerOptions.workerSrc = new URL(
 			"pdfjs-dist/legacy/build/pdf.worker.mjs",
 			import.meta.url,
